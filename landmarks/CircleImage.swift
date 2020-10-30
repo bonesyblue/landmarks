600bfe279f0415cc0589beadd772a7e8823d7dfe
Jonathan Bones <bonesyblue@gmail.com>
2020-10-30 23:00:33 +0100

    refactor: Remove explicit image size modifier

--- landmarks/CircleImage.swift
+++ landmarks/CircleImage.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         Image("tutlerock")
-            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
             .shadow(radius: 10)
