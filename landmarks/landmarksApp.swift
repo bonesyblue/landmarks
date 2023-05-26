3d0f04efeb19f1ea0a7dba1fd18e3244431d9113
Jonathan Bones <bonesyblue@gmail.com>
2023-05-27 00:03:48 +0200

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Jonathan Bones on 27.05.23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
