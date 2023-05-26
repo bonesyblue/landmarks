51aa83f9c6e3baf28c6d7ba78e7784136dc888b6
Jonathan Bones <bonesyblue@gmail.com>
2023-05-27 00:32:21 +0200

    feat: Create a custom image view

---
+++ landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  landmarks
+//
+//  Created by Jonathan Bones on 27.05.23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle()
+                    .stroke(.white, lineWidth: 4)
+                    .shadow(radius: 7)
+            }
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
