7f3ecb41046fdc000006228562dbdf6bef175856
Jonathan Bones <bonesyblue@gmail.com>
2023-05-27 14:08:23 +0200

    feat: Create a basic landmark list

---
+++ landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  landmarks
+//
+//  Created by Jonathan Bones on 27.05.23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
