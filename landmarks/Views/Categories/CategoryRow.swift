01eafd563fe8fed0b27cb1e2444c5ce957eeafa1
Jonathan Bones <bonesyblue@gmail.com>
2023-06-04 13:20:57 +0200

    feat: Create a category row component

---
+++ landmarks/Views/Categories/CategoryRow.swift
@@ -0,0 +1,41 @@
+//
+//  CategoryRow.swift
+//  landmarks
+//
+//  Created by Jonathan Bones on 04.06.23.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark:landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
+    static var previews: some View {
+        CategoryRow(
+            categoryName: landmarks[0].category.rawValue,
+            items: Array(landmarks.prefix(4))
+        )
+    }
+}
