4c3685c7ee84d64902985f7b0f3923bccfef8c0b
Jonathan Bones <bonesyblue@gmail.com>
2023-06-04 13:01:52 +0200

    feat: Add category overview

---
+++ landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  landmarks
+//
+//  Created by Jonathan Bones on 04.06.23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
