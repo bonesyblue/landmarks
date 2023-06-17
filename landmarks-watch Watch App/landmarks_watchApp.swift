e3cd9daa50b63ddfabe54e4859a90dcaa63cc499
Jonathan Bones <bonesyblue@gmail.com>
2023-06-17 22:50:55 +0200

    chore: Add watchOS companion target

---
+++ landmarks-watch Watch App/landmarks_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarks_watchApp.swift
+//  landmarks-watch Watch App
+//
+//  Created by Jonathan Bones on 17.06.23.
+//
+
+import SwiftUI
+
+@main
+struct landmarks_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
